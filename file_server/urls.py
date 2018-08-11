df1a7fbf0a30e9bac4b2978f068a2255b9d414de
Victor Akimov <akimov.victor@gmail.com>
2018-08-11 19:44:15 +0300

    Exercise with commented solution

--- file_server/urls.py
+++ file_server/urls.py
@@ -4,22 +4,28 @@ from django.urls import path, register_converter
 
 from file_server.views import FileList, file
 
-class Date:
-    regex = '[0-9]{4}-[0-9]{2}-[0-9]{2}'
-    format = '%Y-%m-%d'
 
-    def to_python(self, value):
-        return datetime.datetime.strptime(value, self.format).date()
+# Определите и зарегистрируйте конвертер для определения даты в урлах и наоборот урла по датам
 
-    def to_url(self, value: datetime.datetime):
-        return value.strftime(self.format)
-
-
-register_converter(Date, 'date')
+# TODO: убрать решение:
+# class Date:
+#     regex = '[0-9]{4}-[0-9]{2}-[0-9]{2}'
+#     format = '%Y-%m-%d'
+#
+#     def to_python(self, value):
+#         return datetime.datetime.strptime(value, self.format).date()
+#
+#     def to_url(self, value: datetime.datetime):
+#         return value.strftime(self.format)
+#
+#
+# register_converter(Date, 'date')
 
 
 urlpatterns = [
-    path('', FileList.as_view(), name='index'),
-    path('<date:date>', FileList.as_view(), name='index'),
-    path('file/<name>', file, name='file'),
+    # Определите тут схему урлов и привязите к отображениям FileList и files
+    # TODO: убрать решение:
+    # path('', FileList.as_view(), name='index'),
+    # path('<date:date>', FileList.as_view(), name='index'),
+    # path('file/<name>', file, name='file'),
 ]
--- file_server/views.py
+++ file_server/views.py
@@ -14,29 +14,47 @@ class FileList(TemplateView):
     template_name = 'index.html'
     
     def get_context_data(self, date=None):
-        data = super(IndexView, self).get_context_data()
-        dir_content = [os.path.join(settings.FILES_PATH, f)
-                       for f in os.listdir(settings.FILES_PATH)]
-        files = []
-        for path in dir_content:
-            if os.path.isfile(path):
-                st = os.stat(path)
-                ctime = datetime.datetime.fromtimestamp(st.st_ctime)
-                mtime = datetime.datetime.fromtimestamp(st.st_mtime)
-                if date and date not in (ctime.date(), mtime.date()):
-                    continue
-                files.append(FileData(
-                    os.path.basename(path),
-                    path,
-                    ctime,
-                    mtime,
-                ))
-        data['files'] = files
-        data['date'] = date
-        return data
+        # TODO: убрать решение:
+        # data = super(IndexView, self).get_context_data()
+        # dir_content = [os.path.join(settings.FILES_PATH, f)
+        #                for f in os.listdir(settings.FILES_PATH)]
+        # files = []
+        # for path in dir_content:
+        #     if os.path.isfile(path):
+        #         st = os.stat(path)
+        #         ctime = datetime.datetime.fromtimestamp(st.st_ctime)
+        #         mtime = datetime.datetime.fromtimestamp(st.st_mtime)
+        #         if date and date not in (ctime.date(), mtime.date()):
+        #             continue
+        #         files.append(FileData(
+        #             os.path.basename(path),
+        #             path,
+        #             ctime,
+        #             mtime,
+        #         ))
+        # data['files'] = files
+        # data['date'] = date
+        # return data
+
+        # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
+        return {
+            'files': [
+                {'name': 'file_name_1.txt',
+                 'ctime': datetime.datetime(2018, 1, 1),
+                 'mtime': datetime.datetime(2018, 1, 2)}
+            ],
+            'date': datetime.date(2018, 1, 1)  # Этот параметр необязательный
+        }
 
 
 def file(request, name):
-    with open(os.path.join(os.path.join(settings.FILES_PATH, name)), 'r') as f:
-        return render_to_response('file_content.html', context=dict(file_name=name, file_content=f.read()))
+    # TODO: убрать решение:
+    # with open(os.path.join(os.path.join(settings.FILES_PATH, name)), 'r') as f:
+    #     return render_to_response('file_content.html', context=dict(file_name=name, file_content=f.read()))
+
+    # Реализуйте алгоритм подготавливающий контекстные данные для шаблона по примеру:
+    return render_to_response(
+        'file_content.html',
+        context={'file_name': 'file_name_1.txt', 'file_content': 'File content!'}
+    )
 
