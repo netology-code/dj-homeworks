493dd8e1503c3f507bfe48497793b3debe19a968
prokaktus <proktusfahitasiv@gmail.com>
2018-10-30 03:43:45 +0300

    Fix small typos

--- site-form-works/avia_scanner/avia_scanner/settings.py
+++ site-form-works/avia_scanner/avia_scanner/settings.py
@@ -122,6 +122,11 @@ CACHES = {
 }
 
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'your secret key'
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
 try:
     from .settings_local import *
 except ImportError:
