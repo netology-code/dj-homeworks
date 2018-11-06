d88287ba05bd1d4bad9834b08977a3c38778dd34
Alexander Kovalev <ak@alkov.pro>
2018-11-06 03:39:28 +0300

    creating-project tasks ready

---
+++ creating-project/application/app/views.py
@@ -0,0 +1,32 @@
+import csv
+
+from django.shortcuts import render
+from django.views import View
+
+CSV_FILENAME = 'phones.csv'
+COLUMNS = [
+    {'name': 'id', 'width': 1},
+    {'name': 'name', 'width': 3},
+    {'name': 'price', 'width': 2},
+    {'name': 'release_date', 'width': 2},
+    {'name': 'lte_exists', 'width': 1},
+]
+
+
+class TableView(View):
+
+    def get(self, request):
+        with open(CSV_FILENAME, 'rt') as csv_file:
+            header = []
+            table = []
+            table_reader = csv.reader(csv_file, delimiter=';')
+            for table_row in table_reader:
+                if not header:
+                    header = {idx: value for idx, value in enumerate(table_row)}
+                else:
+                    row = {header.get(idx) or 'col{:03d}'.format(idx): value
+                           for idx, value in enumerate(table_row)}
+                    table.append(row)
+
+            result = render(request, 'table.html', {'columns': COLUMNS, 'table': table, 'csv_file': CSV_FILENAME})
+        return result
