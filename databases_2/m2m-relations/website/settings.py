1f4ac9f584c061623dbb3fcd432d1f6d4424cb45
prokaktus <proktusfahitasiv@gmail.com>
2018-11-16 17:42:37 +0300

    Notes about performance

--- databases_2/m2m-relations/website/settings.py
+++ databases_2/m2m-relations/website/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'articles/../static'),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 MEDIA_URL = '/media/'
