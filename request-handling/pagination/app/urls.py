373911580c13c475130d4f3e53000d3f7f7f1769
yar <zexus.x26@gmail.com>
2019-03-17 16:15:59 +0500

    pagination

--- request-handling/pagination/app/urls.py
+++ request-handling/pagination/app/urls.py
@@ -5,5 +5,5 @@ from app.views import index, bus_stations
 
 urlpatterns = [
     path('', index, name='index'),
-    path('bus_stations', bus_stations, name='bus_stations'),
+    path('bus_stations/', bus_stations, name='bus_stations'),
 ]
