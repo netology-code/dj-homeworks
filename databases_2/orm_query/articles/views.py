1734a22757342f523089590897899bc2f4bea305
yar <zexus.x26@gmail.com>
2019-03-30 20:37:44 +0500

    orm_query

--- databases_2/orm_query/articles/views.py
+++ databases_2/orm_query/articles/views.py
@@ -7,3 +7,6 @@ class ArticleListView(ListView):
     template_name = 'articles/news.html'
     model = Article
     ordering = '-published_at'
+
+    def get_queryset(self):
+        return Article.objects.defer('published_at', 'author__phone').select_related('author', 'genre').all()
--- databases_2/orm_query/website/settings.py
+++ databases_2/orm_query/website/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'articles.apps.ArticlesConfig'
+    'articles.apps.ArticlesConfig',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'website.urls'
@@ -129,3 +131,4 @@ try:
 except ImportError:
     pass
 
+INTERNAL_IPS = ['127.0.0.1']
