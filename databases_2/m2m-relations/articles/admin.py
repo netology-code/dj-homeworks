e729696248c5972fe380578a6ff081feaf7eb65c
yar <zexus.x26@gmail.com>
2019-03-30 15:59:38 +0500

    m2m-relations

--- databases_2/m2m-relations/articles/admin.py
+++ databases_2/m2m-relations/articles/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Scope, ArticleScope
+
+
+class ArticleScopeInlineFormset(BaseInlineFormSet):
+
+    def clean(self):
+        main_quantity = 0
+        for form in self.forms:
+            if form.cleaned_data:
+                if form.cleaned_data['is_main'] and not form.cleaned_data['DELETE']:
+                    main_quantity += 1
+        if main_quantity > 1:
+            raise ValidationError('Должен быть только один основной раздел')
+        elif main_quantity == 0:
+            raise ValidationError('Должен быть хотя бы один основной раздел')
+        return super().clean()
+
+
+class ArticleScopeInline(admin.TabularInline):
+    model = ArticleScope
+    extra = 1
+    formset = ArticleScopeInlineFormset
+
+
+@admin.register(Scope)
+class ScopeAdmin(admin.ModelAdmin):
+    inlines = [ArticleScopeInline]
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ArticleScopeInline]
--- databases_2/m2m-relations/articles/models.py
+++ databases_2/m2m-relations/articles/models.py
@@ -14,3 +14,39 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Scope(models.Model):
+
+    topic = models.CharField(max_length=256, verbose_name='Название')
+    articles = models.ManyToManyField(Article, through='ArticleScope')
+
+    class Meta:
+        verbose_name = 'Тема'
+        verbose_name_plural = 'Темы'
+
+    def __str__(self):
+        return self.topic
+
+    @property
+    def is_main(self):
+        return 
+
+
+
+class ArticleScope(models.Model):
+
+    is_main = models.BooleanField(verbose_name='Основной')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='scopes', verbose_name='Статья')
+    scope = models.ForeignKey(Scope, on_delete=models.CASCADE, verbose_name='Раздел')
+
+    class Meta:
+        verbose_name = 'Тематика статьи'
+        verbose_name_plural = 'Тематики статьи'
+
+    def __str__(self):
+        return f'{self.article}-{self.scope}'
+
+    @property
+    def topic(self):
+        return self.scope.topic
