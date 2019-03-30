7faf957ee8616dd9f280c911e3d71c2cb4cd77ff
yar <zexus.x26@gmail.com>
2019-03-30 21:51:49 +0500

    orm_migrations

--- databases_2/orm_migrations/school/views.py
+++ databases_2/orm_migrations/school/views.py
@@ -6,3 +6,6 @@ from .models import Student
 class StudentListView(ListView):
     model = Student
     ordering = 'group'
+
+    def get_queryset(self):
+        return Student.objects.prefetch_related('teachers').all()
