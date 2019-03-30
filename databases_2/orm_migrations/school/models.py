2da4a0e5fb0f1ddc3c8de19d764a395b08079dc2
yar <zexus.x26@gmail.com>
2019-03-30 21:13:17 +0500

    orm_migrations

--- databases_2/orm_migrations/school/models.py
+++ databases_2/orm_migrations/school/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Teacher(models.Model):
     name = models.CharField(max_length=30, verbose_name='Имя')
-    subject = models.CharField(max_length=10, verbose_name='Предмет')
+    subject = models.CharField(max_length=50, verbose_name='Предмет')
 
     class Meta:
         verbose_name = 'Учитель'
@@ -15,7 +15,8 @@ class Teacher(models.Model):
 
 class Student(models.Model):
     name = models.CharField(max_length=30, verbose_name='Имя')
-    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    # teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    teachers = models.ManyToManyField(Teacher, related_name='students')
     group = models.CharField(max_length=10, verbose_name='Класс')
 
     class Meta:
