900f1da52644960a8f609022255d0a419e8ec281
dnovik <dnovik01@gmail.com>
2019-07-29 07:18:12 +0300

    первая часть выполнена

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -12,7 +12,15 @@ counter_click = Counter()
 
 def index(request):
     # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
-    return render_to_response('index.html')
+    if request.GET.get('from-landing') == 'original':
+        counter_click['original']+=1
+        return render_to_response('index.html')
+    elif request.GET.get('from-landing') == 'test':
+        counter_click['test']+=1
+        return render_to_response('index.html')
+    else:
+        return render_to_response('index.html')
+    
 
 
 def landing(request):
@@ -20,7 +28,15 @@ def landing(request):
     # в зависимости от GET параметра ab-test-arg
     # который может принимать значения original и test
     # Так же реализуйте логику подсчета количества показов
-    return render_to_response('landing.html')
+    if request.GET.get('ab-test-arg') == 'original':
+        counter_show['original']+=1
+        return render_to_response('landing.html')
+    elif request.GET.get('ab-test-arg') == 'test':
+        counter_show['test']+=1
+        return render_to_response('landing_alternate.html')
+    else:
+         return render_to_response('landing.html')
+        
 
 
 def stats(request):
@@ -28,7 +44,16 @@ def stats(request):
     # Чтобы отличить с какой версии лендинга был переход
     # проверяйте GET параметр marker который может принимать значения test и original
     # Для вывода результат передайте в следующем формате:
+    try:
+        test_conversion = counter_click['test'] / counter_show['test']
+    except ZeroDivisionError:
+        test_conversion = 0
+    try:    
+        original_conversion = counter_click['original'] / counter_show['original']
+    except ZeroDivisionError:
+        original_conversion = 0
+
     return render_to_response('stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+        'test_conversion': test_conversion,
+        'original_conversion': original_conversion
     })
