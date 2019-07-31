ca0198cf21b9a42b243d06eb602b719585fe3d52
Maxim Filipenko <proktusfahitasiv@gmail.com>
2019-08-01 01:48:27 +0300

    Add note about url structure

--- request-handling/file_server/app/urls.py
+++ request-handling/file_server/app/urls.py
@@ -6,6 +6,7 @@ from django.urls import path
 urlpatterns = [
     # Определите схему урлов с привязкой к отображениям .views.file_list и .views.file_content
     # path(..., name='file_list'),
-    # path(..., name='file_list'),
+    # path(..., name='file_list'),    # задайте необязательный параметр "date"
+                                      # для детальной информации смотрите HTML-шаблоны в директории templates
     # path(..., name='file_content'),
 ]
