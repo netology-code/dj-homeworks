fdb3cfcbd155ac71b0f3b02e583353cee4ad0e78
dnovik <dnovik01@gmail.com>
2019-07-31 14:46:01 +0300

    Сделал третью задачу

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -1,5 +1,24 @@
+import csv
+from app import settings
 from django.shortcuts import render_to_response, redirect
 from django.urls import reverse
+import urllib.parse as urllib
+from django.core.paginator import Paginator
+
+def read_file(file_path=settings.BUS_STATION_CSV):
+    bus_stations = []
+    with open(file_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            bus_stations.append(
+                {
+                    'Name' : row['Name'],
+                    'Street' : row['Street'],
+                    'District' : row['District']
+                }
+            )
+    
+    return bus_stations
 
 
 def index(request):
@@ -7,10 +26,28 @@ def index(request):
 
 
 def bus_stations(request):
-    return render_to_response('index.html', context={
-        'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'}],
-        'current_page': 1,
-        'prev_page_url': None,
-        'next_page_url': 'bus_stations/?page=2',
-    })
 
+    bus_stations_list = read_file()
+    paginator = Paginator(bus_stations_list, 10)
+    page = request.GET.get('page')
+    bus_stations = paginator.get_page(page)
+
+    try:
+        next_page = 'bus_stations?page=' + str(int(page) + 1)
+        prev_page = 'bus_stations?page=' + str(int(page) - 1)
+    except TypeError:
+        page = '1'
+        next_page = 'bus_stations?page=2'
+        prev_page = ''
+    except ValueError:
+        page = '1'
+        next_page = 'bus_stations?page=2'
+        prev_page = ''
+
+
+    return render_to_response('index.html', context={
+        'bus_stations': bus_stations,
+        'current_page': page,
+        'prev_page_url' : '',
+        'next_page_url' : next_page
+    })
