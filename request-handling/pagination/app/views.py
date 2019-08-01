81dfadfa775e5109b8fdbb61d215304e232ef9e0
dnovik <dnovik01@gmail.com>
2019-08-01 16:44:59 +0300

    Fixed

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -32,22 +32,36 @@ def bus_stations(request):
     paginator = Paginator(bus_stations_list, 10)
     page = request.GET.get('page')
     bus_stations = paginator.get_page(page)
+    query = {"page": [page]}
+    link = reverse('bus_stations') + '?' + urllib.urlencode(query, doseq=True)
+
+    print(link)
 
     try:
-        next_page = 'bus_stations?page=' + str(int(page) + 1)
-        prev_page = 'bus_stations?page=' + str(int(page) - 1)
+        prev_page_num = paginator.get_page(page).previous_page_number()
+    except:
+        prev_page_num = 1
+    next_page_num = paginator.get_page(page).next_page_number()
+
+    try:
+        if paginator.get_page(page).has_previous():
+            prev_page = reverse('bus_stations') + '?page=' + str(prev_page_num)
+        else:
+            page = 1
+            prev_page = ''
+
+        if paginator.get_page(page).has_next():
+            next_page = reverse('bus_stations') + '?page=' + str(next_page_num)
+        else:
+            next_page = reverse('bus_stations')
     except TypeError:
-        page = '1'
-        next_page = 'bus_stations?page=2'
-        prev_page = ''
-    except ValueError:
-        page = '1'
-        next_page = 'bus_stations?page=2'
+        page = 1
+        next_page = reverse('bus_stations') + '?page=2'
         prev_page = ''
 
     return render_to_response('index.html', context={
         'bus_stations': bus_stations,
         'current_page': page,
-        'prev_page_url': '',
+        'prev_page_url': prev_page,
         'next_page_url': next_page
     })
