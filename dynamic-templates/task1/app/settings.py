6d53a05338500cabb46a354fa6014060071560ab
dnovik <dnovik01@gmail.com>
2019-08-06 15:31:13 +0300

    Первая задача решена

--- dynamic-templates/task1/app/settings.py
+++ dynamic-templates/task1/app/settings.py
@@ -48,11 +48,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'app.urls'
+TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
+
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -1,10 +1,58 @@
 from django.shortcuts import render
+import csv
+
 
 def inflation_view(request):
     template_name = 'inflation.html'
+    file_path = r'inflation_russia.csv'
+
+    values = []
+
+    with open(file_path, encoding='utf-8') as csv_file:
+        reader = csv.DictReader(csv_file)
+        headers = reader.fieldnames[0].split(';')
+        for line in reader:
+            for value in line.values():
+                values.append(value.split(';'))
+
+    for i, value in enumerate(values):
+        for num, row in enumerate(value):
+            try:
+                if num == 0:
+                    value[num] = str(row)
+                else:
+                    value[num] = float(row)
+            except ValueError:
+                value[num] = row
 
     # чтение csv-файла и заполнение контекста
-    context = {}
+
+    context = {
+        'headers' : headers,
+        'values' : values}
 
     return render(request, template_name,
-                  context)
+                  context=context)
+
+
+
+
+
+'''values = []
+
+with open(file_path, encoding='utf-8') as csv_file:
+    reader = csv.DictReader(csv_file)
+    headers = reader.fieldnames[0].split(';')
+    for line in reader:
+        for value in line.values():
+            values.append(value.split(';'))
+
+for i, value in enumerate(values):
+    for num, row in enumerate(value):
+        try:
+            if num == 0:
+                value[num] = str(row)
+            else:
+                value[num] = float(row)
+        except ValueError:
+            value[num] = row'''
