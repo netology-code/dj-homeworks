b35bdaafdaea9caeaab63cab9758929a0e21e852
prokaktus <proktusfahitasiv@gmail.com>
2019-09-26 02:12:18 +0300

    Add debug notice

--- first-project/first_project/first_project/urls.py
+++ first-project/first_project/first_project/urls.py
@@ -21,6 +21,8 @@ from app.views import home_view
 
 urlpatterns = [
     path('', home_view, name='home'),
+    # Раскомментируйте код, чтобы данные урлы 
+    # обрабатывались Django
     # path('current_time/', time_view, name='time'),
     # path('workdir/', workdir_view, name='workdir'),
     path('admin/', admin.site.urls),
