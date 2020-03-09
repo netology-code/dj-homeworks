b3928c6642f363cb9fc116f06a9b0eeb60e3a34b
prokaktus <proktusfahitasiv@gmail.com>
2020-03-10 01:28:53 +0300

    Fix old url usage

--- databases/work_with_database/main/urls.py
+++ databases/work_with_database/main/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
@@ -22,5 +21,5 @@ import phones.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', phones.views.show_catalog),
-    url(r'^catalog/(?P<slug>[\w-]+)/$', phones.views.show_product),
+    path('catalog/<slug:slug>/', phones.views.show_product),
 ]
