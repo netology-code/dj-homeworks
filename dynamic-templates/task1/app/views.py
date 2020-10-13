2e04952b7c0958846c1cf3a757701474133d36df
4nsw3r <street9507@yandex.ru>
2020-10-13 23:20:34 +0300

    views line #22

--- dynamic-templates/task1/app/views.py
+++ dynamic-templates/task1/app/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render
+import csv
+import io
+
 
 def inflation_view(request):
     template_name = 'inflation.html'
+    rows = []
+    with io.open('inflation_russia.csv', encoding='utf-8') as isfile:
+        file = csv.reader(isfile, delimiter=';')
+        linecount = 0
+        for row in file:
+            if linecount == 0:
+                header = row
+                linecount += 1
+            else:
+                rows.append(row)
+
+        for r in rows:
+            for i, val in enumerate(r[1:]):
+
+                if '.' in val or r[i] is not None:
+                    r[i] = float(r[i])
+
+                #if val == '':
+                 #   r[i] = '-'
+                #if 1 < int(val) < 2:
+                 #   r[i].style
+
 
     # чтение csv-файла и заполнение контекста
-    context = {}
+    context = {'data': rows, 'header': header}
 
     return render(request, template_name,
-                  context)
+                  context=context)
