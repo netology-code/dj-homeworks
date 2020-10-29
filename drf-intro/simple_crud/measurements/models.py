b94296f14f3be565e45b278171ead31a53337326
4nsw3r <street9507@yandex.ru>
2020-10-29 12:55:34 +0300

    dop task#1

--- drf-intro/simple_crud/measurements/models.py
+++ drf-intro/simple_crud/measurements/models.py
@@ -1,28 +1,24 @@
 from django.db import models
 
 
-class Project(models.Model):
+class TimestampFields(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Project(TimestampFields):
     """Объект на котором проводят измерения."""
 
     name = models.TextField()
     latitude = models.FloatField()
     longitude = models.FloatField()
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-    updated_at = models.DateTimeField(
-        auto_now=True
-    )
 
 
-class Measurement(models.Model):
+class Measurement(TimestampFields):
     """Измерение температуры на объекте."""
 
     value = models.FloatField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='meas')
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
-    updated_at = models.DateTimeField(
-        auto_now=True
-    )
