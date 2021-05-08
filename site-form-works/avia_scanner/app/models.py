666fb7a8e429f3395bcdde9769bbcd189b6df83f
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2021-05-08 14:44:42 +0300

    Removed old hws

--- site-form-works/avia_scanner/app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class City(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return f'{self.name}'
--- site-personalization/sessions/game/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def show_home(request):
-    return render(
-        request,
-        'home.html'
-    )
