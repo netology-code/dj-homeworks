9e0f585d5cc140e11e3b23990d6d00c6cff286b4
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2021-08-01 23:54:46 +0300

    Fixed template dirs

--- 2.2-databases-2/m2m-relations/website/settings.py
+++ 2.2-databases-2/m2m-relations/website/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'articles/../templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
