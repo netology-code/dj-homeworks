49e9c1458f23880528e69b5f75738c6c9335f9a2
amurasm <101619423+amurasm@users.noreply.github.com>
2022-03-20 21:16:24 +0400

    задание 2

--- 1.2-requests-templates/recipes/recipes/urls.py
+++ 1.2-requests-templates/recipes/recipes/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 
 from django.urls import path
 
+from calculator.views import calculator
+
 urlpatterns = [
     # здесь зарегистрируйте вашу view-функцию
+    path('calculator/<recipe>/', calculator, name='calculator'),
 ]
