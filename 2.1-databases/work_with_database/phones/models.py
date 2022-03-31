07d453111d2f086a22b8887a7e18f7e59f1a3813
amurasm <101619423+amurasm@users.noreply.github.com>
2022-03-31 08:46:52 +0400

    Задание 2.1

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,19 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Phone(models.Model):
     # TODO: Добавьте требуемые поля
-    pass
+    #id = models.CharField(max_length=10, primary_key=True)
+    name = models.CharField(max_length=50)
+    #slug = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    image = models.CharField(max_length=100)
+    price = models.CharField(max_length=20)
+    release_date = models.CharField(max_length=50)
+    lte_exists = models.CharField(max_length=20)
+    #
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Phone, self).save(*args, **kwargs)
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
+
+from phones.models import Phone
 
 
 def index(request):
@@ -6,12 +9,32 @@ def index(request):
 
 
 def show_catalog(request):
+    phones = Phone.objects.all()
+    sort = request.GET.get('sort')
+    if sort == 'name':
+        phones = phones.order_by("name")
+    elif sort == 'min_price':
+        phones = phones.order_by("price")
+    elif sort == 'max_price':
+        phones = phones.order_by("-price")
+
+    data = [phone.__dict__ for phone in phones]
+
     template = 'catalog.html'
-    context = {}
+    context = {'phones': data}
     return render(request, template, context)
 
 
 def show_product(request, slug):
+    phone_objects = Phone.objects.filter(slug=slug)
+    # print(phone_objects[0].__dict__)
     template = 'product.html'
-    context = {}
+    context = {'phone': phone_objects[0].__dict__}
     return render(request, template, context)
+
+
+def show_db(request):
+    phone_objects = Phone.objects.all()
+    phones = [f'{p.id}: {p.name}, {p.slug}, {p.image}, {p.price}, {p.release_date}, {p.lte_exists}' for p in
+              phone_objects]
+    return HttpResponse('<br>'.join(phones))
