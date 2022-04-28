f1171884091835ebd0a8a05a6967b024e61dc5de
amurasm <101619423+amurasm@users.noreply.github.com>
2022-04-28 10:05:25 +0400

    update -> update_or_create +pagination

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -51,6 +51,8 @@ class StockSerializer(serializers.ModelSerializer):
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
         for position in positions:
-            StockProduct.objects.filter(stock=stock, product=position['product']).update(quantity=position['quantity'],
-                                                                                         price=position['price'])
+            StockProduct.objects.update_or_create(stock=stock, product=position['product'],
+                                                  defaults={'quantity': position['quantity'],
+                                                            'price': position['price']})
+
         return stock
--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -131,3 +131,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 3
+}
