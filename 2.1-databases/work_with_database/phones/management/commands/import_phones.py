2067840b563d04cde97d2f63f8f9c7a23db470e0
Masachusets <apofeoz@tut.by>
2022-08-04 22:30:53 +0300

    database created and filled

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -14,4 +14,9 @@ class Command(BaseCommand):
 
         for phone in phones:
             # TODO: Добавьте сохранение модели
-            pass
+            Phone.objects.create(name=phone['name'],
+                                 price=phone['price'],
+                                 image=phone['image'],
+                                 release_date=phone['release_date'],
+                                 lte_exists=phone['lte_exists'],
+                                 slug=phone['name'])
