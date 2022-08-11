2ea7ba197738d0d4b25a2d2e762948959a2f81fd
Masachusets <apofeoz@tut.by>
2022-08-11 22:56:18 +0300

    realized views, models, admin, db

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
 
-from .models import Article
+from .models import Article, Teg, ArticleTeg
+
+
+class ArticleTegInline(admin.TabularInline):
+    model = ArticleTeg
+    extra = 1
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'title', 'text', 'published_at']
+    list_filter = ['published_at']
+    inlines = [ArticleTegInline, ]
+
+
+@admin.register(Teg)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_filter = ['name']
--- 2.2-databases-2/m2m-relations/articles/views.py
+++ 2.2-databases-2/m2m-relations/articles/views.py
@@ -5,10 +5,9 @@ from articles.models import Article
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
-
+    articles = Article.objects.all()
+    context = {'object_list': articles}
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/3.1/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-    ordering = '-published_at'
 
     return render(request, template, context)
