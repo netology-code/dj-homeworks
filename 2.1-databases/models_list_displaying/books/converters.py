8f5cd83de96bc1c5021011879f22864cd574e9fd
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2022-11-07 20:19:13 +0300

    Update converters.py

--- 2.1-databases/models_list_displaying/books/converters.py
+++ 2.1-databases/models_list_displaying/books/converters.py
@@ -1,10 +1,12 @@
+from datetime import datetime
 
 
-class PubDateConverter:
-    regex = '[0-9]{4}-[0-9]{2}-[0-9]{2}'
+class DateConverter:
+   regex = r'[0-9]{4}-[0-9]{2}-[0-9]{2}'
+   format = '%Y-%m-%d'
 
-    def to_python(self, value):
-        return value
+   def to_python(self, value: str) -> datetime:
+       return datetime.strptime(value, self.format)
 
-    def to_url(self, value):
-        return value.__str__()
+   def to_url(self, value: datetime) -> str:
+       return value.strftime(self.format)
